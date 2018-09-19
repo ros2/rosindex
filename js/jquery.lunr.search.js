04a4013794ae54d7b6fd7bb45f47b640152a0f92
Michel Hidalgo <hid.michel@gmail.com>
2018-09-19 15:31:53 -0300

    Brings back lunr.js and ditches GCSE.
    Plus client-side pagination support.

--- js/jquery.lunr.search.js
+++ js/jquery.lunr.search.js
@@ -17,26 +17,22 @@
     };
   };
 
-  // parse a date in yyyy-mm-dd format
-  var parseDate = function(input) {
-    var parts = input.match(/(\d+)/g);
-    return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
-  };
-
   // define lunr.js search class
   var LunrSearch = (function() {
 
-    function LunrSearch(elem, options) {
-      this.$elem = elem;
-      this.$results = $(options.results);
-      this.$entries = $(options.entries, this.$results);
-      this.template = this.compileTemplate($(options.template));
-      this.ready = options.ready
+  function LunrSearch(input, options) {
+      var self = this;
 
+      this.$input = input;
+      this.$results = $(options.results);
+      this.$pagination = $(options.pagination);
+
+      this.template = this.compileTemplate($(options.template));
+      this.ready = options.ready;
+
+      this.baseUrl = options.baseUrl;
       this.indexUrl = options.indexUrl;
       this.indexDataUrl = options.indexDataUrl;
-
-      var self = this;
 
       this.jxhr = [];
 
@@ -50,28 +46,13 @@
       }));
 
       $.when.apply($, this.jxhr).done(function() {
-        self.populateSearchFromQuery();
-        self.bindKeypress();
-        self.ready();
-        console.log("done loading everything");
+          self.populateSearchFromQuery();
+          self.resetSearchResults();
+          self.bindKeypress();
+          self.ready();
+          console.log("done loading everything");
       });
     }
-
-      // http://stackoverflow.com/questions/5817811/jquery-multiple-getjson-requests
-
-    // create lunr.js search index
-    LunrSearch.prototype.createIndex = function() {
-      return lunr(function() {
-        this.field('name', { boost: 30 });
-        this.field('tags', { boost: 20 });
-        this.field('description', { boost: 10 });
-        this.field('readme');
-        this.field('maintainers');
-        this.field('authors');
-        this.field('distro');
-        this.ref('id');
-      });
-    };
 
     // Compile search results template
     LunrSearch.prototype.compileTemplate = function($template) {
@@ -82,109 +63,56 @@
       };
     };
 
-    // load the search index data
-    LunrSearch.prototype.loadIndexData = function(callback) {
-    };
-
-    LunrSearch.prototype.loadIndex = function(callback) {
-    };
-
-    LunrSearch.prototype.populateIndex = function(data) {
-      var index = this.index;
-
-      // format the raw json into a form that is simpler to work with
-      this.entries = $.map(data.entries, this.createEntry);
-
-      // add each entry to the index
-      $.each(this.entries, function(idx, entry) {
-        index.add(entry);
-      });
-    };
-
-    // reformat the raw json into a simpler format
-    LunrSearch.prototype.createEntry = function(raw, index) {
-      // add the index from jQuery.map to this object
-      // this will be used as a unique identifier for the object
-      // this index is 1-based
-      var entry = $.extend({
-        id: index + 1
-      }, raw);
-
-      // include pub date for posts
-      if (raw.date) {
-        //$.extend(entry, {
-          //date: parseDate(raw.date),
-          //pubdate: function() {
-            //// HTML5 pubdate
-            //return dateFormat(parseDate(raw.date), 'yyyy-mm-dd');
-          //},
-          //displaydate: function() {
-            //// only for posts (e.g. Oct 12, 2012)
-            //return dateFormat(parseDate(raw.date), 'mmm dd, yyyy');
-          //}
-        //});
-      }
-
-      return entry;
-    };
-
-    // Bind the keup to the search function
-    //
-    // Only re-calls the search function when the value changes and subject to
-    // debounce
+    // Bind keyup events to search results refreshes.
     LunrSearch.prototype.bindKeypress = function() {
       var self = this;
-      var oldValue = this.$elem.val();
 
-      this.$elem.bind('keyup', debounce(function() {
-        var newValue = self.$elem.val();
+      var oldValue = self.$input.val();
+      self.$input.bind('keyup', debounce(function() {
+        var newValue = self.$input.val();
         if (newValue !== oldValue) {
-          self.search(newValue);
+          self.resetSearchResults();
         }
-
         oldValue = newValue;
       }));
     };
 
-    // Search function which calls lunr and displays results
-    //
-    // If the query is too short, it hides the results table, and clears the entries
-    // If the query is long enough, it calls lunr and maps the
-    LunrSearch.prototype.search = function(query) {
-      var entries = this.entries;
-
-      if (query.length < 2) {
-        this.$results.hide();
-        this.$entries.empty();
-      } else {
-        // for each search result, grep all the entries for the entry which correpsonds to the result reference
-        var results = $.map(this.index.search(query), function(result) {
-          return $.grep(entries, function(entry) { return entry.id === parseInt(result.ref, 10); })[0];
+    LunrSearch.prototype.resetSearchResults = function() {
+        var self = this;
+        self.$pagination.pagination({
+            dataSource: function(done) {
+                done(self.search(self.$input.val()));
+            },
+            callback: function(entries, pagination) {
+                var have_entries = (entries.length > 0);
+                self.$results.html(
+                    self.template({
+                        entries: entries,
+                        have_entries: have_entries,
+                        baseurl: self.baseUrl
+                    })
+                );
+            },
+            ulClassName: "pagination pagination-sm",
+            pageSize: 10
         });
-
-        this.displayResults(results);
-      }
     };
 
-    // Render the results
-    //
-    // This will either add a notice or render a template based on the result
-    // entries
-    LunrSearch.prototype.displayResults = function(entries) {
-      var $entries = this.$entries,
-          $results = this.$results;
-
-      // clear out the entries
-      $entries.empty();
-
-      if (entries.length === 0) {
-        $entries.append('<tr><td class="text-center>Nothing found.</td><tr>');
-      } else {
-        $entries.append(this.template({entries: entries}));
+    // Search function that leverages lunr. If the query is too short
+    // (i.e. less than 2 characters long), no search is performed.
+    LunrSearch.prototype.search = function(query) {
+      var self = this;
+      if (query.length < 2) {
+        // Too short of a query, skip.
+        return [];
       }
-
-      // show the results element if it isn't already visible
-      $results.show();
+      // For each search result, grep all the entries for the entry
+      // which corresponds to the result reference
+      return $.map(this.index.search(query), function(result) {
+          return $.grep(self.entries, function(entry) {
+              return entry.id === parseInt(result.ref, 10);
+          })[0];
+      });
     };
 
     // Populate the search input with 'q' querystring parameter if set
@@ -193,14 +121,12 @@
       var queryString = uri.search(true);
 
       if (queryString.hasOwnProperty('q')) {
-        this.$elem.val(queryString.q);
-        this.search(queryString.q.toString());
+        this.$input.val(queryString.q);
       }
     };
 
     return LunrSearch;
   })();
-
 
   $.fn.lunrSearch = function(options) {
     // apply default options
@@ -212,11 +138,14 @@
     return this;
   };
 
-
   $.fn.lunrSearch.defaults = {
-    indexUrl  : '/search.json',     // Url for the .json file containing search index source data (containing: title, url, date, body)
-    results   : '#search-results',  // selector for containing search results element
-    entries   : '.entries',         // selector for search entries containing element (contained within results above)
-    template  : '#search-results-template'  // selector for Mustache.js template
+    baseUrl: '',                 // Base url for search results links.
+    indexUrl: '/index.json',     // Url for the .json file containing the
+                                 // search index.
+    indexDataUrl: '/search.json', // Url for the json file containing search
+                                  // data.
+    pagination: '#search-pagination',  // Selector for pagination container
+    results: '#search-results',  // Selector for results container
+    template: '#search-results-template'  // Selector for Mustache.js template
   };
 })(jQuery);
