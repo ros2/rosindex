c6df2b04d01da548263c610c82a4075ea6b70360
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2018-10-01 17:09:31 -0300

    Addresses peer review comments

--- _plugins/pulling_docs.rb
+++ _plugins/pulling_docs.rb
@@ -4,16 +4,16 @@ require 'fileutils'
 
 Jekyll::Hooks.register :site, :after_init do |site|
     site.config['docs_repos'].each do |repo|
-        src = File.join(site.source, "_remotes", repo["name"])
+        src = File.join(site.source, "_remotes", repo[0])
         dest_dir = File.join(site.source, "doc")
-        dest = File.join(dest_dir, repo["name"])
+        dest = File.join(dest_dir, repo[0])
         if File.directory?(dest)
           FileUtils.rm_r(dest)
         end
         if File.directory?(src)
             FileUtils.cp_r(src, dest_dir)
         else
-            raise IOError.new("Unable to copy from #{origin}, the directory doesn't exist.")
+            raise IOError.new("Unable to copy from #{src}, the directory doesn't exist.")
         end
     end
 end
