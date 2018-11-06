02cd9820682ff0cb65cb0845aa1670546ac22106
Michel Hidalgo <hid.michel@gmail.com>
2018-11-06 19:04:33 -0300

    Uses the newest available distro on first package or repo page load.

--- _plugins/pre_configuration.rb
+++ _plugins/pre_configuration.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :site, :after_init  do |site|
-  site.config['distros'] = site.config['ros_distros'] +
-                           site.config['ros2_distros']
-  site.config['old_distros'] = site.config['old_ros_distros'] +
-                               site.config['old_ros2_distros']
+  site.config['distros'] = site.config['ros2_distros'] +
+                           site.config['ros_distros']
+  site.config['old_distros'] = site.config['old_ros2_distros'] +
+                               site.config['old_ros_distros']
 end
