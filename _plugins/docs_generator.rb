9ae9cbe9840f4e5293af012e8fbf9765b6d9ac3b
Gonzalo de Pedro <gonzalo@depedro.com.ar>
2018-11-22 16:36:58 -0300

    Make the docs repositories sphinx repositories. References #112 and ros2/ros2_documentation#10

--- _plugins/docs_generator.rb
+++ _plugins/docs_generator.rb
@@ -76,7 +76,7 @@ class DocPageGenerator < Jekyll::Generator
   end
 
   def convert_with_sphinx(repo_name, repo_data)
-    repo_path = Pathname.new(File.join("_remotes", repo_name))
+    repo_path = Pathname.new(File.join("_remotes", "#{repo_name}/source/"))
     in_path = Pathname.new(File.join('_sphinx', 'repos', repo_name))
     FileUtils.rm_r(in_path) if File.directory? in_path
     copied_docs_paths = copy_docs(repo_path, in_path)
