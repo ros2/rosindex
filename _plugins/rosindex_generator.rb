443fe5754e7baa8869c03697f1eb5507e44608e5
Tully Foote <tfoote@osrfoundation.org>
2020-10-14 16:16:10 -0700

    add support for referencing en prefix on docs.ros.org

--- _plugins/rosindex_generator.rb
+++ _plugins/rosindex_generator.rb
@@ -30,6 +30,7 @@ require_relative '../_ruby_libs/lunr'
 
 $fetched_uris = {}
 $debug = false
+DEFAULT_LANGUAGE_PREFIX = 'en'
 
 def get_ros_api(elem)
   return []
@@ -217,7 +218,7 @@ class Indexer < Jekyll::Generator
 
   def get_ci_data(distro, package_name, repo_name)
     ci_data = Hash.new
-    manifest_url = '/'+distro+'/api/'+package_name+'/manifest.yaml'
+    manifest_url = '/'+DEFAULT_LANGUAGE_PREFIX+'/'+distro+'/api/'+package_name+'/manifest.yaml'
     manifest_response = Net::HTTP.get_response('docs.ros.org', manifest_url)
     if manifest_response.code != '200'
       ci_data['tooltip'] = 'No CI information available for this package.'
@@ -238,7 +239,7 @@ class Indexer < Jekyll::Generator
     end
     ci_data['job_url'] = manifest_yaml['devel_jobs'][0]
     # get additional test information if available
-    results_url = '/'+distro+'/devel_jobs/'+repo_name+'/results.yaml'
+    results_url = '/'+DEFAULT_LANGUAGE_PREFIX+'/'+distro+'/devel_jobs/'+repo_name+'/results.yaml'
     results_response = Net::HTTP.get_response('docs.ros.org', results_url)
     if results_response.code != '200'
       ci_data['tooltip'] = "Latest build information: " + ci_data['timestamp'] + "\n" \
@@ -495,7 +496,7 @@ class Indexer < Jekyll::Generator
       end
 
       if $ros_distros.include? distro
-        docs_uri = "http://docs.ros.org/#{distro}/api/#{package_name}/html/"
+        docs_uri = "http://docs.ros.org/#{DEFAULT_LANGUAGE_PREFIX}/#{distro}/api/#{package_name}/html/"
       else
         docs_uri = "http://docs.ros2.org/#{distro}/api/#{package_name}/"
       end
